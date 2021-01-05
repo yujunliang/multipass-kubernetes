40c32c5ded599c578fce8ca04b29b1caf5b370d1
Your Name <you@example.com>
2021-01-04 23:17:09 -0600

    Added kubeadm init.

--- cks-master/install.sh
+++ cks-master/install.sh
@@ -24,4 +24,4 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"

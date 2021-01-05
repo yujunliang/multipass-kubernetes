74af4fa8471f2bca828776ce51d71dcf159cc772
Your Name <you@example.com>
2021-01-04 23:09:17 -0600

    Added kubeadm init.

--- cks-master/install.sh
+++ cks-master/install.sh
@@ -16,3 +16,12 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
 
+kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+

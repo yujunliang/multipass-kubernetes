ed9c0239a48d7cfab0a01821c0cc22efdbc7db55
taewoo.kim <taewdy@gmail.com>
2022-11-29 08:55:37 +1100

    update

--- multipass/launch-2vm.sh
+++
@@ -1,3 +0,0 @@
-multipass launch --name cks-master -c 2 -m 2G
-multipass launch --name cks-worker -c 2 -m 2G
-
---
+++ multipass/launch-4vm.sh
@@ -0,0 +1,4 @@
+multipass launch --name k8s-master -c 2 -m 2G -d 15G
+multipass launch --name k8s-node1 -c 2 -m 2G -d 15G
+multipass launch --name k8s-node1 -c 2 -m 2G -d 15G
+multipass launch --name k8s-node1 -c 2 -m 2G -d 15G

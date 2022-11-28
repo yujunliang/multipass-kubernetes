1a967462673148abe13c918cbf27d9e78b547a94
taewoo.kim <taewdy@gmail.com>
2022-11-29 09:08:12 +1100

    update

--- multipass/launch-4vm.sh
+++ multipass/launch-4vm.sh
@@ -1,4 +1,4 @@
 multipass launch --name k8s-master -c 2 -m 2G -d 15G
 multipass launch --name k8s-node1 -c 2 -m 2G -d 15G
-multipass launch --name k8s-node1 -c 2 -m 2G -d 15G
-multipass launch --name k8s-node1 -c 2 -m 2G -d 15G
+multipass launch --name k8s-node2 -c 2 -m 2G -d 15G
+multipass launch --name k8s-node3 -c 2 -m 2G -d 15G
